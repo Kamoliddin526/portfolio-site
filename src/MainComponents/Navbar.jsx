0287bf4eb8d3afcbae8f1b90e3b12f0073937d62
Kamoliddin <kuliyevkamoliddin5@gmail.com>
2023-07-31 09:23:40 +0500

    aedsd

--- src/MainComponents/Navbar.jsx
+++ src/MainComponents/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "../App.scss";
 import { Link } from "react-router-dom";
+import Slide from 'react-reveal/Slide';
 
 const Nav = () => {
   return (
     <div className="nav">
+      <Slide top>
       <div className="logo">
         {" "}
         <h2>trxvl.</h2>{" "}
       </div>
+      </Slide>
+      <Slide top cascade>
       <div className="menu">
         <Link to={"/"}>Home</Link>
         <Link to={"/page_2"}>Stays</Link>
@@ -16,6 +20,7 @@ const Nav = () => {
         <Link>Packages</Link>
         <Link>Sign Up</Link>
       </div>
+      </Slide>
     </div>
   );
 };
